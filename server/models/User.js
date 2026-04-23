e0f1dd6d07967036adcdd74a3a00e7dde653b56e
shresthwithouta <shresthwithouta@gmail.com>
2026-04-23 16:00:16 +0530

    fix: remove railway.json and fix password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -34,7 +34,7 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true })
 
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next()
+  if (!this.isModified('password') || !this.password) return next()
   this.password = await bcrypt.hash(this.password, 10)
   next()
 })
