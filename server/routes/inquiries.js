697bb54bc748b12fa161c044462898ebdc362fca
rupam905 <rupamdutta905.com>
2026-04-27 14:57:07 +0530

    inquiries.js: replace PATCH /:id/status with PUT /:id for full inquiry updates

--- server/routes/inquiries.js
+++ server/routes/inquiries.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import { getInquiries, createInquiry, updateInquiryStatus, deleteInquiry } from '../controllers/inquiryController.js'
+import { getInquiries, createInquiry, updateInquiry, deleteInquiry } from '../controllers/inquiryController.js'
 import { protect, adminOnly } from '../middleware/auth.js'
 
 const router = Router()
 
 router.get('/', protect, adminOnly, getInquiries)
 router.post('/', createInquiry)
-router.patch('/:id/status', protect, adminOnly, updateInquiryStatus)
+router.put('/:id', protect, adminOnly, updateInquiry)
 router.delete('/:id', protect, adminOnly, deleteInquiry)
 
 export default router
