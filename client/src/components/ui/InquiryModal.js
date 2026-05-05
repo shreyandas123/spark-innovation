03722991301a45f44d4c018a15b77cb28fa735ed
shresthwithouta <shresthwithouta@gmail.com>
2026-05-05 09:40:15 +0530

    Bugfix: Import useEffect in InquiryModal to resolve ReferenceError

--- client/src/components/ui/InquiryModal.js
+++ client/src/components/ui/InquiryModal.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { X, CheckCircle, Loader2, Send } from "lucide-react";
 import { createInquiry } from "@/lib/api";
 
