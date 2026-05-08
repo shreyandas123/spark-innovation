c43d148811e802dae21895ba760df139727ae31c
shresthwithouta <shresthwithouta@gmail.com>
2026-05-08 13:46:07 +0530

    fix

--- client/src/components/ui/SmoothScroll.js
+++ client/src/components/ui/SmoothScroll.js
@@ -5,10 +5,10 @@ import { ReactLenis } from "lenis/react";
 export default function SmoothScroll({ children }) {
   return (
     <ReactLenis root options={{ 
-      duration: 1.8, 
+      duration: 1.2, 
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      wheelMultiplier: 0.8,
+      wheelMultiplier: 1.1,
       touchMultiplier: 1.5,
       infinite: false,
     }}>
